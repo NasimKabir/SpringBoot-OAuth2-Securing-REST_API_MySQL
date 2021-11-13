cc0970afdb4def6243946543413f0c40d9b283d9
Nasim Kabir <mdkabir334@gmail.com>
2021-11-13 21:30:00 +0600

    initial stage or fresh project

---
+++ src/test/java/com/spring/SecurityWithOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWithOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
